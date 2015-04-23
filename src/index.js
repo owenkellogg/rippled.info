41c9ae269b6a41e9324b83740d5cdf36368c73e1
Steven Zeiler <zeiler.steven@gmail.com>
2015-04-23 09:03:38 -0700

    [FEATURE] Display domain images and ips lists

--- src/index.js
+++ src/index.js
@@ -9,13 +9,18 @@ App.addRegions({
 
 var Rippled = Backbone.Model.extend({
   parse: function(ip) {
+    console.log('parse rippled', ip)
     return { ip: ip }
   }
 })
 
 var Rippleds = Backbone.Collection.extend()
 
-var Domain = Backbone.Model.extend()
+var Domain = Backbone.Model.extend({
+  defaults: {
+    imageUrl: ""
+  }
+})
 
 var Domains = Backbone.Collection.extend({
   model: Domain
@@ -31,8 +36,15 @@ var DomainsListView = Backbone.Marionette.CollectionView.extend({
   tagName: 'ul'
 })
 
+var RippledListItemView = Backbone.Marionette.ItemView.extend({
+  template: _.template('<li><%= ip %></li>')
+})
+
 var domains = new Domains(Object.keys(data.rippleds).map(function(key) {
-  return { domain: key }
+  return {
+    domain: key,
+    imageUrl: data.rippleds[key].imageUrl
+  }
 }))
 
 var domainsListView = new DomainsListView({
@@ -41,6 +53,7 @@ var domainsListView = new DomainsListView({
 
 var DomainView = Backbone.Marionette.CompositeView.extend({
   template: '#domain-template',
+  childView: RippledListItemView,
   childViewContainer: 'ul'
 })
 
@@ -50,16 +63,19 @@ var Router = Backbone.Marionette.AppRouter.extend({
     "domains/:domain" : "showDomain"
   },
   showDomain : function(domain){
-    var model = new Domain({ domain: domain })
+    var model = new Domain({
+      domain: domain,
+      imageUrl: data.rippleds[domain].imageUrl
+    })
+    var rippleds = data.rippleds[domain]["ips"].map(function(ip) {
+      return { ip: ip }
+    })
+    var collection = new Rippleds(rippleds)
     var domainView = new DomainView({
       model: model,
-      collection: new Rippleds(data.rippleds[domain]),
-      templateHelpers: function() {
-        return { item: model.toJSON() }
-      }
+      collection: collection
     })
     App.mainRegion.show(domainView)
-    console.log(data.rippleds[domain])
   },
   home: function() {
     var domainsListView = new DomainsListView({
